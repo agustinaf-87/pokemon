3d0d1fdd91bec55f3e9034ec29ce7e6da6400fda
agustinaf-87 <fernandez.agustinaelena@gmail.com>
2023-07-05 16:59:39 +0200

    uploading changes on pokemon home page

--- src/app/shared/pipes/poke-image.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PokeImagePipe } from "./poke-image.pipe";
-
-describe("PokeImagePipe", () => {
-  it("create an instance", () => {
-    const pipe = new PokeImagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
